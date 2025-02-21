3d27b3faf769ab6fa9fc6aed17c9f055e237aa02
Ryu Takahashi <107231834+ryu-thakahashi@users.noreply.github.com>
2025-02-21 18:03:23 +0900

    Add db_url delete codd

--- settings.py
+++ settings.py
@@ -8,6 +8,9 @@ SESSION_CONFIGS = [
     ),
 ]
 
+if "DATABASE_URL" in environ:
+    del environ["DATABASE_URL"]
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
